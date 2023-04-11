28ce5740bb9d7deb71abd68448c95c5482968808
shawon <shawon.softeng@gmail.com>
2023-04-11 20:27:50 +0600

    add by mr shawon

--- react-data-thinking/src/App.jsx
+++ react-data-thinking/src/App.jsx
@@ -1,69 +1,199 @@
 import React from "react";
-
-const task = {
-    id: "unique_id_001",
-    subtitle: "Subtitle",
-    createdAt: "2023-04-10T13:27:29.024Z",
-    tags: [
-        {
-            id: "tag_001",
-            text: "Its done",
-            icon: "T",
-        },
-        {
-            id: "tag_002",
-            text: "Its cancelled",
-            icon: "X",
-        },
-        {
-            id: "tag_003",
-            text: "Its in progress",
-            icon: "p",
-        },
-        {
-            id: "tag_004",
-            text: "Just Wrote this",
-            icon: "C",
-        },
-    ],
-    comments: [
-        {
-            id: "comment_id_001",
-            user: {
-                avatar: "xyx.com",
-                name: "Viraj",
-                id: "user_id_001",
+import "./App.css";
+const tasks = [
+    {
+        id: "unique_id_001",
+        subtitle: "Subtitle",
+        createdAt: "2023-04-10T13:27:29.024Z",
+        tags: [
+            {
+                id: "tag_001",
+                text: "Its done",
+                icon: "T",
             },
-            text: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, voluptates.",
-        },
-    ],
-    tasks: [
-        {
-            id: "task_001",
-            title: "Foggy Nelson",
-            text: "Here to clean the streets of Hell's kitchen ",
-            status: "done",
-        },
-        {
-            id: "task_002",
-            title: "Louise CK",
-            text: "This one is cancelled",
-            status: "cancel",
-        },
-        {
-            id: "task_003",
-            title: "Albert Einstein",
-            text: "In Progress",
-            status: "progress",
-        },
-        {
-            id: "task_004",
-            title: "Albert Einstein",
-            text: "In Progress",
-            status: "progress",
-        },
-    ],
-};
+            {
+                id: "tag_002",
+                text: "Its cancelled",
+                icon: "X",
+            },
+            {
+                id: "tag_003",
+                text: "Its in progress",
+                icon: "p",
+            },
+            {
+                id: "tag_004",
+                text: "Just Wrote this",
+                icon: "C",
+            },
+        ],
+        comments: [
+            {
+                id: "comment_id_001",
+                user: {
+                    avatar: "xyx.com",
+                    name: "Viraj",
+                    id: "user_id_001",
+                },
+                text: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, voluptates.",
+            },
+        ],
+        tasks: [
+            {
+                id: "task_001",
+                title: "Foggy Nelson",
+                text: "Here to clean the streets of Hell's kitchen ",
+                status: "done",
+            },
+            {
+                id: "task_002",
+                title: "Louise CK",
+                text: "This one is cancelled",
+                status: "cancel",
+            },
+            {
+                id: "task_003",
+                title: "Albert Einstein",
+                text: "In Progress",
+                status: "progress",
+            },
+            {
+                id: "task_004",
+                title: "Albert Einstein",
+                text: "In Progress",
+                status: "progress",
+            },
+        ],
+    },
+    {
+        id: "unique_id_001",
+        subtitle: "Subtitle",
+        createdAt: "2023-04-10T13:27:29.024Z",
+        tags: [
+            {
+                id: "tag_001",
+                text: "Its done",
+                icon: "T",
+            },
+            {
+                id: "tag_002",
+                text: "Its cancelled",
+                icon: "X",
+            },
+            {
+                id: "tag_003",
+                text: "Its in progress",
+                icon: "p",
+            },
+            {
+                id: "tag_004",
+                text: "Just Wrote this",
+                icon: "C",
+            },
+        ],
+        comments: [
+            {
+                id: "comment_id_001",
+                user: {
+                    avatar: "xyx.com",
+                    name: "Viraj",
+                    id: "user_id_001",
+                },
+                text: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, voluptates.",
+            },
+        ],
+        tasks: [
+            {
+                id: "task_001",
+                title: "Foggy Nelson",
+                text: "Here to clean the streets of Hell's kitchen ",
+                status: "done",
+            },
+            {
+                id: "task_002",
+                title: "Louise CK",
+                text: "This one is cancelled",
+                status: "cancel",
+            },
+            {
+                id: "task_003",
+                title: "Albert Einstein",
+                text: "In Progress",
+                status: "progress",
+            },
+            {
+                id: "task_004",
+                title: "Albert Einstein",
+                text: "In Progress",
+                status: "progress",
+            },
+        ],
+    },
+    {
+        id: "unique_id_001",
+
+        createdAt: "2023-04-10T13:27:29.024Z",
+        tags: [
+            {
+                id: "tag_001",
+                text: "Its done",
+                icon: "T",
+            },
+            {
+                id: "tag_002",
+                text: "Its cancelled",
+                icon: "X",
+            },
+            {
+                id: "tag_003",
+                text: "Its in progress",
+                icon: "p",
+            },
+            {
+                id: "tag_004",
+                text: "Just Wrote this",
+                icon: "C",
+            },
+        ],
+        comments: [
+            {
+                id: "comment_id_001",
+                user: {
+                    avatar: "xyx.com",
+                    name: "Viraj",
+                    id: "user_id_001",
+                },
+                text: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, voluptates.",
+            },
+        ],
+        tasks: [
+            {
+                id: "task_001",
+                title: "Foggy Nelson",
+                text: "Here to clean the streets of Hell's kitchen ",
+                status: "done",
+            },
+            {
+                id: "task_002",
+                title: "Louise CK",
+                text: "This one is cancelled",
+                status: "cancel",
+            },
+            {
+                id: "task_003",
+                title: "Albert Einstein",
+                text: "In Progress",
+                status: "progress",
+            },
+            {
+                id: "task_004",
+                title: "Albert Einstein",
+                text: "In Progress",
+                status: "progress",
+            },
+        ],
+    },
+];
 
 function getDay(dateStr) {
     const date = new Date(dateStr).getDay();
@@ -85,24 +215,72 @@ function formateDate(dateStr) {
     return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 }
 
-const App = () => {
+const TagListItem = ({ tag }) => {
+    console.log(tag);
     return (
-        <div>
-            <h2>
+        <li key={tag.id}>
+            {tag.icon} - {tag.text}
+        </li>
+    );
+};
+
+const CommentListItem = ({ comment }) => {
+    return (
+        <div className="comment-item" key={comment.id}>
+            <h4> {comment.user.name} </h4>
+            <p>{comment.text} </p>
+        </div>
+    );
+};
+
+const TaskCard = ({ task }) => {
+    return (
+        <div className="day-card">
+            <h2 className="title">
                 {getDay(task.createdAt)}, {formateDate(task.createdAt)}
             </h2>
 
-            <h3> {task.subtitle} </h3>
+            <h3 className="sub-title"> {task.subtitle} </h3>
 
-            <div>
+            <div className="tag-ul">
                 {task.tags.map((tag) => {
-                    <li key={tag.id}>
-                        {tag.icon} - {tag.text}
-                    </li>;
+                    <TagListItem key={tag.id} tag={tag} />;
                 })}
             </div>
-            <hr />
-            <h4> Notes linked to people </h4>
+            <div className="line"></div>
+            <h4 className="notes"> Notes linked to people </h4>
+
+            <div className="comments">
+                {task.comments.map((comment) => {
+                    <CommentListItem comment={comment} />;
+                })}
+            </div>
+
+            <ul className="tasks">
+                {task.tasks.map((task) => {
+                    <TaskListItem key={task.id} task={task} />;
+                })}
+            </ul>
+        </div>
+    );
+};
+const TaskListItem = ({ task }) => {
+    return (
+        <li key={task.id}>
+            <h3> {task.title} </h3>
+            <p>
+                <small>{task.title} </small>
+            </p>
+            <p>{task.text}</p>
+        </li>
+    );
+};
+const App = () => {
+    return (
+        <div className="cards-group">
+            {tasks.map((task) => (
+                <TaskCard key={task.id} task={task} />
+            ))}
         </div>
     );
 };
